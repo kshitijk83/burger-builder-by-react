c1b071039b05bd7f09713b68e0a8d32a21dcdaaf
kshitij kumar <kshitijk83@gmail.com>
2019-01-05 15:57:12 +0530

    making starter files(checkout page) for routing

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './Containers/Checkout/Checkout';
+
 class App extends Component {
 
   render() {
     return (
       <div className="App">
         <Layout>
-          <BurgerBuilder />:null
+          <BurgerBuilder />
+          <Checkout />
         </Layout>
       </div>
     );
